221e70dd909db0e0106fff1f3d968d9298817460
alexvolodin <166716843+alexvolodin@users.noreply.github.com>
2024-04-23 14:18:41 +0300

    Commit#1

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, pal!");
-        System.out.println("once more +++");
+        System.out.println("once more");
     }
 }
